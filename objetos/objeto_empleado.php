70c99b71ffb123a581dbf13e214699e70923dc0f
BenjaminSk09 <159175209+BenjaminSk09@users.noreply.github.com>
2024-07-19 11:00:44 -0600

    Objeto empleado

--- objetos/objeto_empleado.php
+++ objetos/objeto_empleado.php
@@ -1,18 +1,17 @@
 <?php
     include_once('../clases/Empleado.php');
     $emple = new Empleado();
-    //atributos de la clase Empleado    
-    $emple->codigo=123;
-    $emple->nivelAcademico="Técnico FullStack";
-    $emple->sueldoBase=1000;
-
     //atributos que hereda de la clase Persona
-    $emple->asignarNombre('Fulanito');
-    $emple->apellido="de Tal";
-    $emple->telefono=12345678;
-    $emple->direccion="Zona 5";
+    $emple->asignarNombre($_GET['txtNombre']);
+    $emple->apellido=$_GET['txtApellido'];
+    $emple->telefono=$_GET['txtTelefono'];
+    $emple->direccion=$_GET['txtDireccion'];
 
-    echo "Sueldo liquido: ". $emple->calcularSueldoLiquido();
+    //atributos de la clase Empleado    
+    $emple->codigo=$_GET['txtCodigo'];
+    $emple->nivelAcademico=$_GET['txtNivelAcademico'];
+    $emple->sueldoBase=$_GET['txtSueldoBase'];
+
     echo $emple->verDatos();
     
 ?>
